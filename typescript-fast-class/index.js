2b9dd18c0d2a5734e724021bb5cf501a85634e87
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-08-14 17:13:13 +0600

    add enum type

--- typescript-fast-class/index.js
+++ typescript-fast-class/index.js
@@ -1,2 +1,86 @@
 "use strict";
-const myName = 'Sakhawat Hossain';
+// // Any, number, string,, boolean,, object, array, tuple, enum, undefined, null, void, never, unknown
+// let myName: string = 'Sakhawat Hossain'
+// let age: number = 10
+// let isHungry: boolean = false;
+// let studentId: number | string = 371;
+// studentId = 'shakib-371'
+// type StringOrNumber = number | string
+// let studentId: StringOrNumber = 371;
+// studentId = 'shakib-371'
+// studentId = 49641
+// let person: object = {
+//     name : 'Shakib',
+//     age : 21,
+//     hobby: 'chatting'
+// };
+// type PersonType = { name: string, age: number, hobby: string }
+// let person: PersonType = {
+//     name : 'Shakib',
+//     age : 21,
+//     hobby : 'chatting'
+// }
+// let person01: PersonType = {
+//     name : 'Shakib',
+//     age : 21,
+//     hobby : 'chatting'
+// }
+// interface Preson {
+//     name: string;
+//     age: number;
+//     hobby: string;
+// }
+// let person: Preson = {
+//     name : 'Shakib',
+//     age : 21,
+//     hobby : 'chatting'
+// }
+// let person01: Preson = {
+//     name : 'Shakib',
+//     age : 21,
+//     hobby : 'chatting'
+// }
+// // Array
+// const numbers: (number | string)[] = [1, 2, 3, 4, 5] 
+// const persons: object[] = [
+//     {
+//         name: "Solaimen Shadin",
+//         age : 21,
+//         hobby : 'chatting'
+//     },
+//     {
+//         name: "Solaimen Shadin",
+//         age : 21,
+//         hobby : 'chatting'
+//     },
+//     {
+//         name: "Solaimen Shadin",
+//         age : 21,
+//         hobby : 'chatting'
+//     }
+// ]
+// // FUCNTIONS
+// const greeting = (name: string): void => {
+//     console.log(`Hello ${name}`);
+// }
+// greeting("Shadin")
+// const introduce = (person: Preson): void => {
+//     console.log(`Helllo my name is ${person.name
+//     } I Am ${person.age}`);
+// }
+// introduce({
+//     name: 'Shakib',
+//     age : 36,
+//     hobby: "Solaimen"
+// })
+var Week;
+(function (Week) {
+    Week["Sat"] = "Sun";
+    Week["Sun"] = "Sun";
+    Week["Mon"] = "Mon";
+    Week["Tue"] = "Tue";
+    Week["Wed"] = "Wed";
+    Week["Thr"] = "Thr";
+    Week["Fri"] = "Fri";
+})(Week || (Week = {}));
+console.log(Week.Mon);
--- typescript-fast-class/index.ts
+++ typescript-fast-class/index.ts
@@ -20,20 +20,94 @@
 //     hobby: 'chatting'
 // };
 
-type PersonType = { name: string, age: number, hobby: string }
+// type PersonType = { name: string, age: number, hobby: string }
 
-let person: PersonType = {
-    name : 'Shakib',
-    age : 21,
-    hobby : 'chatting'
+// let person: PersonType = {
+//     name : 'Shakib',
+//     age : 21,
+//     hobby : 'chatting'
+// }
+
+// let person01: PersonType = {
+//     name : 'Shakib',
+//     age : 21,
+//     hobby : 'chatting'
+// }
+
+
+
+// interface Preson {
+//     name: string;
+//     age: number;
+//     hobby: string;
+// }
+
+// let person: Preson = {
+//     name : 'Shakib',
+//     age : 21,
+//     hobby : 'chatting'
+// }
+
+// let person01: Preson = {
+//     name : 'Shakib',
+//     age : 21,
+//     hobby : 'chatting'
+// }
+
+
+// // Array
+
+
+// const numbers: (number | string)[] = [1, 2, 3, 4, 5] 
+// const persons: object[] = [
+//     {
+//         name: "Solaimen Shadin",
+//         age : 21,
+//         hobby : 'chatting'
+//     },
+//     {
+//         name: "Solaimen Shadin",
+//         age : 21,
+//         hobby : 'chatting'
+//     },
+//     {
+//         name: "Solaimen Shadin",
+//         age : 21,
+//         hobby : 'chatting'
+//     }
+// ]
+
+// // FUCNTIONS
+
+// const greeting = (name: string): void => {
+//     console.log(`Hello ${name}`);
+    
+// }
+
+// greeting("Shadin")
+
+
+// const introduce = (person: Preson): void => {
+//     console.log(`Helllo my name is ${person.name
+//     } I Am ${person.age}`);
+// }
+
+// introduce({
+//     name: 'Shakib',
+//     age : 36,
+//     hobby: "Solaimen"
+// })
+
+
+enum Week {
+    Sat = 'Sun',
+    Sun = 'Sun',
+    Mon = 'Mon',
+    Tue = 'Tue',
+    Wed = 'Wed',
+    Thr = 'Thr',
+    Fri = 'Fri',
 }
 
-let person01: PersonType = {
-    name : 'Shakib',
-    age : 21,
-    hobby : 'chatting'
-}
-
-
-
+console.log(Week.Mon);
 
