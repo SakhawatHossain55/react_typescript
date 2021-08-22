3e2352cb569d38a684dc729e302a72fa771f0afc
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-08-22 14:09:10 +0600

    react typescript todo

---
+++ react__typescript__todo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Contacts from './componetns/Contacts';
+
+function App() {
+  return (
+    <div>
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
