c3d1220fa45c350abbca11a03a0b8895744136f1
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-08-22 15:34:43 +0600

    tyescript type check

---
+++ typescript__larn_with__sumit/output/script.js
@@ -0,0 +1,28 @@
+"use strict";
+var country = "I love Bangladesh";
+console.log(country);
+var playerName = "Masharafi";
+console.log(playerName);
+var age = 34;
+console.log(age);
+var playerName1;
+playerName1 = "Masharafi";
+playerName1 = 34;
+// console.log(playerName1);
+// function
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(5, 4));
+// console.log(multiply("hello" , 4));      // error
+// array
+var fruits = ['apple', 'orange', 'banana'];
+fruits.push('multa');
+// fruits.push(45)      // error
+// object
+var person = {
+    name: 'oishani',
+    age: 45,
+    isCaptain: true
+};
+// person.country = 'United States'         // error
---
+++ typescript__larn_with__sumit/src/script.ts
@@ -0,0 +1,41 @@
+const country = "I love Bangladesh";
+console.log(country);
+
+let playerName = "Masharafi"
+console.log(playerName);
+
+let age = 34;
+console.log(age);
+
+let playerName1;
+playerName1 = "Masharafi"
+playerName1 = 34
+// console.log(playerName1);
+
+// function
+function multiply(a: number, b: number) {
+    return a * b;
+}
+
+console.log(multiply(5 , 4));
+// console.log(multiply("hello" , 4));      // error
+
+// array
+
+let fruits = ['apple', 'orange', 'banana']
+fruits.push('multa')
+// fruits.push(45)      // error
+
+
+// object
+
+let person = {
+    name: 'oishani',
+    age: 45,
+    isCaptain: true
+}
+
+// person.country = 'United States'         // error
+
+
+
